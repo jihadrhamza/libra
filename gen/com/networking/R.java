fec1988dd4cc15d9c70499abbfdf890f25de4756
Anas Abubacker <anasabubacker@Anass-MacBook-Pro.local>
2014-05-17 00:42:31 +0530

    merge

--- gen/com/networking/R.java
+++ gen/com/networking/R.java
@@ -8,7 +8,7 @@ package com.networking;
 
 public final class R {
 	public static final class drawable {
-		public static final int ic_launcher = 0x7f02003e;
+		public static final int ic_launcher = 0x7f020042;
 	}
 	public static final class id {
 		public static final int textView = 0x7f050030;
